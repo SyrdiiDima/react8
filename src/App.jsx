fe268e32aa5af2f6f7b2ee81db37b403664a97a9
SyrdiiDima <dimasyrdij@icloud.com>
2022-09-02 17:41:19 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Container } from 'components/Container/Container';
-import { ContactsPage } from 'pages/ContactsPage';
-import { AppBar } from 'components/AppBar/AppBar';
-// import { HomePage } from 'pages/HomePage';
-// import { RegisterPage } from 'pages/RegisterPage';
-// import { LoginPage } from 'pages/LoginPage';
+
 import { authOperations } from 'redux/auth/auth-operations';
-import PrivateRoute from 'components/PrivateRoute';
-import PublicRoute from 'components/PublicRoute';
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { Container } from 'components/Container/Container';
+import { ContactsPage } from 'pages/ContactsPage';
+import { AppBar } from 'components/AppBar/AppBar';
+
+import PrivateRoute from 'components/PrivateRoute';
+import PublicRoute from 'components/PublicRoute';
 const HomePage = lazy(() => import('./pages/HomePage'));
 const RegisterPage = lazy(() => import('./pages/RegisterPage'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
